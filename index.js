5d449daecdde0fa837ae49f0506826fe21c2eeb2
JasonS <jason@infostarproductions.com>
2019-04-18 15:16:54 -0700

    hitting the coindesk endpoint, getting json

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+new Vue({
+  el: '#app',
+  data()  {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then( response  => (this.info = response ))
+    }
+})
