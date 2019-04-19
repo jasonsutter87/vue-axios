25943cd80246a114c415299317b3e151a4000511
JasonS <jason@infostarproductions.com>
2019-04-18 17:01:07 -0700

    added filter for decimal point

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ new Vue({
       info: null
     }
   },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
+  },
   mounted () {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
